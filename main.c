3ff3b708506524e042795e8cfa6a74a0ebdd6e9d
İsmail Hakkı Turan <147559619+ismailhakkituran@users.noreply.github.com>
2024-03-12 10:51:06 +0300

    Calculation Template Added

--- main.c
+++ main.c
@@ -1,5 +1,30 @@
 #include<stdio.h>
+
+void aOa-3D{
+	/*
+	*
+	* Please add a code block here that will start 
+	* the calculation after messages have been received from
+	* all anchors.
+	*
+	*/
+
+
+	/* START THE 3D-AoA CALCULATION*/
+
+
+	/* PRINT THE 3D-AoA CALCULATION RESULT
+	*  IN POLAR COORDINATES
+	*/
+	
+	/* r1, r2, \theta, \sigma */
+
+}
 int main( void )
 {
+	aOa-3D(distance1, anchor1_x, anchor1_y, anchor1_z); //Anchor_1_Call
+	aOa-3D(distance2, anchor2_x, anchor2_y, anchor2_z); //Anchor_2_Call
+	aOa-3D(distance3, anchor3_x, anchor3_y, anchor3_z); //Anchor_3_Call
+	aOa-3D(distance4, anchor4_x, anchor4_y, anchor4_z); //Anchor_4_Call
 	return 0;
 }
